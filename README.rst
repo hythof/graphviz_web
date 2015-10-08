69d6a77d28c62742cc4aa9ba961b28368bd08838
tadokoro <hiroshi@websoft.jp>
2015-10-08 19:16:13 +0900

    first release

---
+++ README.rst
@@ -0,0 +1,39 @@
+Graphviz on WEB
+===============
+
+This is GraphvizWeb.
+It will be drawn in real time as you enter text.
+
+.. image:: https://raw.githubusercontent.com/hythof/graphviz_web/master/docs/example.png
+    :alt: Browser image
+    :width: 844
+    :height: 716
+    :align: center
+
+
+Install(Ubuntu)
+---------------
+::
+
+    sudo apt-get install graphviz
+    sudo apt-get install git
+    sudo apt-grt install golang
+    git clone https://github.com/hythof/graphviz_web.git
+    cd graphviz_web/src/
+    go run server.go
+
+
+Access the browser
+http://localhost:8888/
+
+
+Reference
+---------
+Graphviz - Graph Visualization Software
+http://www.graphviz.org/
+
+
+License
+-------
+GPL v3
+https://raw.githubusercontent.com/hythof/graphviz_web/master/LICENSE
---
+++ src/server.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+var (
+	logger *log.Logger
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	file, err := os.Open("index.html")
+	if err != nil {
+		logger.Println(err.Error())
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, err = io.Copy(w, file)
+	if err != nil {
+		logger.Println(err.Error())
+		fmt.Fprintf(w, err.Error())
+	}
+}
+
+func update(w http.ResponseWriter, r *http.Request) {
+	dot := r.FormValue("dot")
+	cmd := exec.Command("dot", "-Tpng")
+	cmd.Stdin = strings.NewReader(dot)
+
+	w.Header().Set("Content-Type", "image/png")
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		logger.Println(err.Error())
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+	defer stdout.Close()
+
+	err = cmd.Start()
+	if err != nil {
+		logger.Println(err.Error())
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+	_, err = io.Copy(w, stdout)
+	if err != nil {
+		logger.Println(err.Error())
+		return
+	}
+	err = cmd.Wait()
+	if err != nil {
+		logger.Println(err.Error())
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+}
+
+func main() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+	logger = log.New(os.Stdout, "", log.LstdFlags|log.Llongfile)
+	http.HandleFunc("/api/graphviz", update)
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8888", nil)
+}
